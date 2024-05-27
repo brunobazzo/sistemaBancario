8acc68c6c291bacbe1670b144a5f37e3de787c8a
Bruno Bazzo <bruno.bazzo@hotmail.com>
2024-05-27 18:06:02 -0300

    nova versao

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -3,47 +3,115 @@ menu = """
 [d] Depositar
 [s] Sacar
 [e] Extrato
+[c] Cadastrar usuario
+[b] Consultar usuario
+[a] Criar conta
 [q] Sair
 
 => """
 
-
-saldo = 0
-limite = 500
-extrato_list = []
-
-numero_saques = 0
+# nome, data de nascimento, cpf, endereco
+usuarios = []
+# numero, cpf, agencia, saldo, extrato, numero de saques, limite
+contas = []
 LIMITE_SAQUES = 3
 
-def depositar(valor_deposito):
-    global saldo, extratextrato_listo
-    if valor_deposito <= 0:
-        print(f"voce nao pode depositar o valor {valor_deposito}")
-    else:
-        saldo += valor_deposito
-        extrato_list.append("R$"+str(valor_deposito))
+def padroniza_cpf(cpf):
+    return int(cpf.replace(".","").replace("-",""))
 
-def sacar(valor_saque):
-    global saldo, numero_saques, extrato_list
-    if numero_saques == LIMITE_SAQUES:
-        print(f"voce atingiu o numero de saques diario maximo de {LIMITE_SAQUES}")
-    elif valor_saque > limite:
-        print(f"voce nao pode sacar o valor {valor_saque} pois é maior que limite{limite}")
-    elif valor_saque > saldo:
-        print(f"voce nao pode sacar o valor {valor_saque} pois é maior que saldo {saldo}")
-    else:
-        numero_saques += 1
-        saldo -= valor_saque
-        extrato_list.append("-R$"+str(valor_saque))
+def criar_conta(cpf):
+    global usuarios
+    global contas
+    
+    cpf = padroniza_cpf(cpf)
+
+    for usuario in usuarios:
+        print (usuario[0])
+        if usuario[0] == cpf:
+            agencia = "0001"
+            contas.append([f"{len(contas)+1:03d}", cpf, agencia, 0,[],0,500])
+            break
+
+    print (contas)
 
 
-def extrato():
-    global saldo
-    if extrato_list:
-        print(f"extrato: {extrato_list}")
-    else:
-        print(f"Nao foram realizadas movimentações")
-    print(f"seu saldo é de R$: {saldo:.2f}")
+def consultar_cadastro_usuario(cpf):
+
+    global usuarios
+
+    cpf = padroniza_cpf(cpf)
+    
+    for usuario in usuarios:
+        if usuario[0] == cpf:
+            print(f"usuario ja cadastrado {usuario}")
+            return True
+
+    print(f"usuario nao cadastrado")
+    return False
+
+
+
+def cadastrar_usuario(cpf):
+    global usuarios
+
+    cpf = padroniza_cpf(cpf)
+
+    nome = input("Digite nome: ")
+    cidade = input("Digite a cidade: ")
+
+    usuarios.append([cpf, nome, cidade])
+
+    print(usuarios)
+
+def depositar(valor_deposito,numero_conta):
+    global contas
+
+    for conta in contas:
+        if conta[0] == numero_conta:
+            if valor_deposito <= 0:
+                print(f"voce nao pode depositar o valor {valor_deposito}")
+                break
+            else:
+                print(f"vai depositar")
+                conta[3] += int(valor_deposito)
+                conta[4].append("+R$"+str(valor_deposito))
+                break
+     
+    print(contas)
+
+
+def sacar(valor_saque,numero_conta):
+
+    global contas
+
+    for conta in contas:
+        if conta[0] == numero_conta:
+
+
+            if conta[5] == LIMITE_SAQUES:
+                print(f"voce atingiu o numero de saques diario maximo de {LIMITE_SAQUES}")
+            elif valor_saque > conta[6]:
+                print(f"voce nao pode sacar o valor {valor_saque} pois é maior que limite{conta[6]}")
+            elif valor_saque > conta[3]:
+                print(f"voce nao pode sacar o valor {valor_saque} pois é maior que saldo {conta[3]}")
+            else:
+                conta[5] += 1
+                conta[3] -= valor_saque
+                conta[4].append("-R$"+str(valor_saque))
+                break
+
+
+def extrato(conta):
+    global contas
+
+    for conta in contas:
+        if conta[0] == numero_conta:
+            if conta[4]:
+                print(f"extrato: {conta[4]}")
+            else:
+                print(f"Nao foram realizadas movimentações")
+            print(f"seu saldo é de R$: {conta[3]:.2f}")
+            break
 
 
 
@@ -52,17 +120,31 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
+        numero_conta = str(input("Digite o número da conta: "))
         valor_deposito = input("Digite o valor de deposito: ")
-        depositar(int(valor_deposito))
+        depositar(int(valor_deposito),numero_conta)
 
 
     elif opcao == "s":
+        numero_conta = str(input("Digite o número da conta: "))
         valor_saque = input("Digite o valor do saque: ")
-        sacar(int(valor_saque))
+        sacar(numero_conta=numero_conta, valor_saque=int(valor_saque))
 
+    elif opcao == "c":
+        cpf = input("Digite o CPF: ")
+        if consultar_cadastro_usuario(cpf) == False:
+            cadastrar_usuario(cpf)
+    elif opcao == "b":
+        cpf = input("Digite o CPF: ")
+        consultar_cadastro_usuario(cpf)
+
+    elif opcao == "a":
+        cpf = input("Digite o CPF: ")
+        criar_conta(cpf)
 
     elif opcao == "e":
-        extrato()
+        numero_conta = str(input("Digite o número da conta: "))
+        extrato(conta=numero_conta)
 
     elif opcao == "q":
         break
